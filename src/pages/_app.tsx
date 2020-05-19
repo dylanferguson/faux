ae5e2924683115cfeef513605fe07a21f661f790
Dylan Ferguson <dylan.b.ferguson@gmail.com>
2020-05-20 00:42:04 +1000

    feat: add init web vitals reporter
    still need to send to analytics provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,15 @@ import React from "react";
 import App from "next/app";
 import "./tailwind.css";
 
-import { Layout } from "../components/Layout";
+import { Layout } from "components/Layout";
+
+// TODO: Relay to analytics provider
+// https://nextjs.org/docs/advanced-features/measuring-performance
+export function reportWebVitals(metric: any) {
+  if (process.env.NODE_ENV === "production") {
+    console.log(metric);
+  }
+}
 
 class MyApp extends App {
   render() {
