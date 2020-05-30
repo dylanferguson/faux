ba40370c42ad229b8b7dface789f36d0766e7f6a
Dylan Ferguson <dylan.b.ferguson@gmail.com>
2020-05-30 22:03:45 +1000

    add Contentful RichTextRenderer options types

--- src/lib/contentful.tsx
+++ src/lib/contentful.tsx
@@ -1,21 +1,36 @@
 import { createClient } from "contentful";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import { Document, BLOCKS, MARKS } from "@contentful/rich-text-types";
+import {
+  Document,
+  BLOCKS,
+  MARKS,
+  Block,
+  Inline,
+} from "@contentful/rich-text-types";
 
 export const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID as string,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
 });
 
-const options = {
+type RichTextRenderOptions = {
   renderNode: {
-    [BLOCKS.PARAGRAPH]: (_, children) => <p>{children}</p>,
-    [BLOCKS.HEADING_1]: (_, children) => <h1>{children}</h1>,
-    [BLOCKS.HEADING_2]: (_, children) => <h2>{children}</h2>,
-    [BLOCKS.HEADING_3]: (_, children) => <h3>{children}</h3>,
-    [BLOCKS.HEADING_4]: (_, children) => <h4>{children}</h4>,
-    [BLOCKS.HEADING_5]: (_, children) => <h5>{children}</h5>,
-    [BLOCKS.HEADING_6]: (_, children) => <h6>{children}</h6>,
+    [key: string]: (
+      node: Block | Inline,
+      children: React.ReactNode
+    ) => React.ReactNode;
+  };
+};
+
+const options: RichTextRenderOptions = {
+  renderNode: {
+    [BLOCKS.PARAGRAPH]: (node, children) => <p>{children}</p>,
+    [BLOCKS.HEADING_1]: (node, children) => <h1>{children}</h1>,
+    [BLOCKS.HEADING_2]: (node, children) => <h2>{children}</h2>,
+    [BLOCKS.HEADING_3]: (node, children) => <h3>{children}</h3>,
+    [BLOCKS.HEADING_4]: (node, children) => <h4>{children}</h4>,
+    [BLOCKS.HEADING_5]: (node, children) => <h5>{children}</h5>,
+    [BLOCKS.HEADING_6]: (node, children) => <h6>{children}</h6>,
   },
 };
 
