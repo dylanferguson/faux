700884b6ab5f14258e34f38ba29abc42a8462b1c
Dylan Ferguson <dylan.b.ferguson@gmail.com>
2020-05-30 22:37:31 +1000

    add temp basic home page

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import { Header } from "components/organisms";
+
+export default () => (
+  <Header title="Phở! Blog">
+    News, tips and highlights from the team at <span className="logo" />
+  </Header>
+);
