83f732578320208d9198230f6ecee0e401e486ff
Dylan Ferguson <dylan.b.ferguson@gmail.com>
2020-06-01 23:46:49 +1000

    simplify PostCSS dev config, apply transformations only on prod builds

--- postcss.config.js
+++ postcss.config.js
@@ -1,26 +1,32 @@
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    ...(process.env.NODE_ENV === "production"
-      ? {
-          "@fullhuman/postcss-purgecss": {
+  plugins: isProd
+    ? [
+        "tailwindcss",
+        "autoprefixer",
+        [
+          "@fullhuman/postcss-purgecss",
+          {
             content: ["./src/components/**/*.tsx", "./src/pages/**/*.tsx"],
             defaultExtractor: (content) =>
               content.match(/[\w-/:]+(?<!:)/g) || [],
           },
-        }
-      : {}),
-    "postcss-import": {},
-    "postcss-flexbugs-fixes": {},
-    "postcss-preset-env": {
-      autoprefixer: {
-        flexbox: "no-2009",
-      },
-      stage: 3,
-      features: {
-        "custom-properties": false,
-      },
-    },
-  },
+        ],
+        "postcss-import",
+        "postcss-flexbugs-fixes",
+        [
+          "postcss-preset-env",
+          {
+            autoprefixer: {
+              flexbox: "no-2009",
+            },
+            stage: 3,
+            features: {
+              "custom-properties": false,
+            },
+          },
+        ],
+      ]
+    : ["tailwindcss"],
 };
