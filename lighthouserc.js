9706d70b6fbf84e42da0cf2e4b8efe81f01afb16
Dylan Ferguson <dylan.b.ferguson@gmail.com>
2020-06-02 21:29:21 +1000

    upload Lighthouse CI audit results to LH server

---
+++ lighthouserc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  ci: {
+    collect: {
+      numberOfRuns: 1,
+      staticDistDir: "./out",
+      url: [
+        "http://localhost/",
+        "http://localhost/blog",
+        "http://localhost/login",
+        "http://localhost/release-notes",
+        "http://localhost/credit-cards/new",
+      ],
+    },
+    upload: {
+      target: "lhci",
+      serverBaseUrl: "https://vast-taiga-80570.herokuapp.com/",
+      token: process.env.LHCI_BUILD_TOKEN,
+    },
+  },
+};
