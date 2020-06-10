49c9b69a14c97e53893738ad9ea1eeb62230cc33
Dylan Ferguson <dylan.b.ferguson@gmail.com>
2020-06-11 03:32:12 +1000

    tidy /index route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import { Header } from "components/organisms";
+import Link from "next/link";
 
 export default () => (
-  <Header title="Faux! Blog">
-    News, tips and highlights from the team at <span className="logo" />
+  <Header title="Faux! A Headless CMS POC">
+    See{" "}
+    <Link href="/blog/">
+      <a>/Blog</a>
+    </Link>{" "}
+    for an overview of the architecture, the technical choices made and the
+    benefits of a Headless CMS-based stack
   </Header>
 );
