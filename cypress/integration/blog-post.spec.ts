28d819f09bcca97699ea55125a61dbafe7731199
Dylan Ferguson <dylan.b.ferguson@gmail.com>
2020-06-16 18:11:34 +1000

    run Lighthouse CI against preview URL

--- cypress/integration/blog-post.spec.ts
+++ cypress/integration/blog-post.spec.ts
@@ -1,6 +1,6 @@
 describe("Blog post", () => {
   it("should render post", () => {
-    cy.visit("/blog/part-i-current-state-of-the-service");
+    cy.visit("/blog/benefits/");
     cy.get("[data-cy='blog-post']").scrollIntoView().should("be.visible");
   });
 });
--- lighthouserc.js
+++ lighthouserc.js
@@ -1,13 +1,7 @@
-const host = "http://localhost";
-
 module.exports = {
   ci: {
     collect: {
       numberOfRuns: 5,
-      staticDistDir: "./out",
-      url: ["/", "/blog", "/login", "/release-notes", "/credit-cards/new"].map(
-        (url) => host + url
-      ),
     },
     upload: {
       target: "lhci",
