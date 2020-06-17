033ee1bbf87d308680d1d49f76dc15cc28af23d7
Dylan Ferguson <dylan.b.ferguson@gmail.com>
2020-06-17 20:47:05 +1000

    only upload to LH server on commits to master, upload to temp public storage on PRs

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,6 +5,13 @@ module.exports = {
     },
     upload: {
       target: "lhci",
+      url: [
+        "https://faux.digital/",
+        "https://faux.digital/blog",
+        "https://faux.digital/login",
+        "https://faux.digital/release-notes",
+        "https://faux.digital/cards",
+      ],
     },
   },
 };
