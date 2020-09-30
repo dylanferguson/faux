88a9daeb5d8f1194ab2e7c0069beadffa2d6e2d0
Dylan Ferguson <dylan.b.ferguson@gmail.com>
2020-09-30 21:54:08 +1000

    fix: cypress config update, simplify img component

--- src/components/atoms/Image.tsx
+++ src/components/atoms/Image.tsx
@@ -1,29 +1,8 @@
-import { useState } from "react";
-import classNames from "classnames";
-
 type ImageProps = {
   src: string;
   alt?: string;
 };
 
-export const Image = ({ src, alt = "" }: ImageProps) => {
-  const [modal, setModal] = useState(false);
-
-  return (
-    <div
-      className={classNames({
-        "fixed flex items-center justify-center top-0 left-0 z-30 w-screen h-screen bg-opacity-50 bg-black": modal,
-      })}
-    >
-      <img
-        className={classNames("rounded-lg cursor-pointer", {
-          "py-4 max-h-screen w-auto": modal,
-        })}
-        loading="lazy"
-        alt={alt}
-        src={src}
-        onClick={() => setModal(!modal)}
-      />
-    </div>
-  );
-};
+export const Image = ({ src, alt = "" }: ImageProps) => (
+  <img className="rounded-lg" loading="lazy" alt={alt} src={src} />
+);
