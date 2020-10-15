2cf88f9c8a20a24aacf45659f7448416834e8987
Dylan Ferguson <dylan.b.ferguson@gmail.com>
2020-10-15 21:03:12 +1100

    swap out React for Preact

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 const withSourceMaps = require("@zeit/next-source-maps")({
   devtool: "hidden-source-map",
 });
+const withPrefresh = require('@prefresh/next');
 
 const {
   NEXT_PUBLIC_SENTRY_DSN: SENTRY_DSN,
@@ -13,8 +14,12 @@ const {
   NODE_ENV,
 } = process.env;
 
-module.exports = withBundleAnalyzer(
+const config = withBundleAnalyzer(
   withSourceMaps({
+    experimental: {
+      modern: true,
+      polyfillsOptimization: true
+    },
     env: {
       NEXT_PUBLIC_VERSION: require("./package.json").version,
       NEXT_PUBLIC_BUILD_TIME: new Intl.DateTimeFormat("en-AU", {
@@ -29,7 +34,7 @@ module.exports = withBundleAnalyzer(
       }).format(new Date()),
     },
     exportTrailingSlash: true,
-    webpack: (config, options) => {
+    webpack: (config, { buildId, dev, isServer }) => {
       // When all the Sentry configuration env variables are available/configured
       // The Sentry webpack plugin gets pushed to the webpack plugins to build
       // and upload the source maps to sentry.
@@ -47,12 +52,47 @@ module.exports = withBundleAnalyzer(
             include: ".next",
             ignore: ["node_modules"],
             urlPrefix: "~/_next",
-            release: `${require("./package.json").version}-${options.buildId}`,
+            release: `${require("./package.json").version}-${buildId}`,
           })
         );
+      }
+
+      const splitChunks = config.optimization && config.optimization.splitChunks;
+
+      if (splitChunks) {
+        const cacheGroups = splitChunks.cacheGroups;
+        const preactModules = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/;
+        if (cacheGroups.framework) {
+          cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
+            test: preactModules
+          });
+          cacheGroups.commons.name = 'framework';
+        }
+        else {
+          cacheGroups.preact = {
+            name: 'commons',
+            chunks: 'all',
+            test: preactModules
+          };
+        }
+      }
+
+      // Install webpack aliases:
+      const aliases = config.resolve.alias || (config.resolve.alias = {});
+      aliases.react = aliases['react-dom'] = 'preact/compat';
+
+      // inject Preact DevTools
+      if (dev && !isServer) {
+        const entry = config.entry;
+        config.entry = () => entry().then(entries => {
+          entries['main.js'] = ['preact/debug'].concat(entries['main.js'] || []);
+          return entries;
+        });
       }
 
       return config;
     },
   })
 );
+
+module.exports = withPrefresh(config);
