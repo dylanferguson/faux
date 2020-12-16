265f60eac13c8ce4849e051b25bb18ef0f13c664
Dylan Ferguson <dylan.b.ferguson@gmail.com>
2020-12-16 23:00:13 +1100

    preload some font resources

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -48,7 +48,32 @@ class MyDocument extends Document {
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
+
           <link href="/fonts/fira-sans/fira-sans.css" rel="stylesheet" />
+          <link
+            rel="preload"
+            href="/fonts/fira-sans/fira-sans-v10-latin-regular.woff2"
+            as="font"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/fira-sans/fira-sans-v10-latin-italic.woff2"
+            as="font"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/fira-sans/fira-sans-v10-latin-900.woff2"
+            as="font"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/fira-sans/fira-sans-v10-latin-900italic.woff2"
+            as="font"
+            crossOrigin="anonymous"
+          />
 
           <link
             rel="preconnect"
