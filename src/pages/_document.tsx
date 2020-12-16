7d43916057281abb8704caed59246e25538e8a78
Dylan Ferguson <dylan.b.ferguson@gmail.com>
2020-12-16 22:38:44 +1100

    :wave: google fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -48,20 +48,12 @@ class MyDocument extends Document {
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
+          <link href="/fonts/fira-sans/fira-sans.css" rel="stylesheet" />
 
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com/"
-            crossOrigin=""
-          />
           <link
             rel="preconnect"
             href="https://www.googletagmanager.com"
             crossOrigin=""
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@300;400;500;600;700;900&display=swap&subset=latin"
-            rel="stylesheet"
           />
           {process.env.NODE_ENV === "production" && (
             <>
