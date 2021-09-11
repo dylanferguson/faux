9d0e73fe2b0b365ebe677b95883caadaa475ae86
Dylan Ferguson <dylan.b.ferguson@gmail.com>
2021-09-11 12:31:45 +1000

    chore: lhci updates, Heroku deploy fixes

--- lhci-server/server.js
+++ lhci-server/server.js
@@ -1,11 +1,5 @@
-/**
- * @license Copyright 2019 Google Inc. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
- */
-'use strict';
-
-const {createServer} = require('@lhci/server');
+const { createServer } = require('@lhci/server');
+const process = require('process');
 
 console.log('Starting server...');
 createServer({
@@ -14,6 +8,14 @@ createServer({
     storageMethod: 'sql',
     sqlDialect: 'postgres',
     sqlConnectionSsl: true,
+    // Unfortunately, Heroku free-tier does not have verifiable certificates.
+    // See https://help.heroku.com/3DELT3RK/why-can-t-my-third-party-utility-connect-to-heroku-postgres-with-ssl for why.
+    sqlDialectOptions: {ssl: {rejectUnauthorized: false}},
     sqlConnectionUrl: process.env.DATABASE_URL,
   },
-}).then(({port}) => console.log('Listening on port', port));
+})
+  .then(({ port }) => console.log('Listening on port', port))
+  .catch((error) => {
+    console.error('Error starting Lighthouse server:', error);
+    process.exit(1);
+  })
